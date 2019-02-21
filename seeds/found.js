6cac87d6394f041a20bdfbe3bb22e045a40c1395
HarryCandlish <hcnd90@gmail.com>
2019-02-21 16:26:14 +1300

    built lost and found databases

--- seeds/found.js
+++ seeds/found.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('found').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('found').insert([
+        {id: 1, species: 'labrador', photo: 'url', user_id: 1},
+        {id: 2,  species: 'tabby', photo: 'url', user_id: 2},
+        {id: 3, species: 'pointer', photo: 'url', user_id: 3}
       ]);
     });
 };
--- seeds/lost.js
+++ seeds/lost.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('lost').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('lost').insert([
+        {id: 1, name: 'bob', species: 'labrador', photo: 'url', user_id: 1},
+        {id: 2,  name: 'alice', species:'tabby', photo: 'url', user_id: 2},
+        {id: 3, name: 'worm', species: 'pointer', photo: 'url', user_id: 3}
       ]);
     });
 };
