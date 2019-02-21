cbb2c2519f806730bdb20850c214f1eac64dece0
ranjit03 <ranjitendurthi03@gmail.com>
2019-02-22 02:04:28 +1300

    /api/lost is working

--- server/routes/lostAndFound.js
+++ server/routes/lostAndFound.js
@@ -1,20 +1,18 @@
 const express = require("express");
 const router = express.Router();
-// const {getLostPets} = require('../db/db')
-// const {getFoundCat} = require('../db/db')
+const db = require("../db/db");
 
 // Get /api/lost
 
 router.get("/lost", (req, res) => {
-  // getLostPets()
-  //   .then(lostPets => {
-  //     res.json(lostPets);
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //     res.status(500)({ error: "something went wrong" });
-  //   });
-  res.send("hi");
+  db.getLostPets()
+    .then(lostPets => {
+      res.json(lostPets);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500)({ error: "something went wrong" });
+    });
 });
 
 module.exports = router;
