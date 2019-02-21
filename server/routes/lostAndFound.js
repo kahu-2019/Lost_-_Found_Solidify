6255ea6fc27640d8d51fbc8b894dc89165b3f948
ranjit03 <ranjitendurthi03@gmail.com>
2019-02-22 01:51:34 +1300

    fixed route

--- server/routes/lostAndFound.js
+++ server/routes/lostAndFound.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const {getLostPets} = require('../db/db')
-const {getFoundCat} = require('../db/db')
+const express = require("express");
+const router = express.Router();
+// const {getLostPets} = require('../db/db')
+// const {getFoundCat} = require('../db/db')
 
 // Get /api/lost
 
-    router.get('/', (req, res) => {
-        getLostPets()
-          .then(lostPets => {
-              res.json(lostPets)
-          })
-        .catch(err => {
-            console.log(err)
-              res.status(500)({error:'something went wrong'})
-        })
-    })
+router.get("/lost", (req, res) => {
+  // getLostPets()
+  //   .then(lostPets => {
+  //     res.json(lostPets);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.status(500)({ error: "something went wrong" });
+  //   });
+  res.send("hi");
+});
 
-    module.export = routes
+module.exports = router;
