c7d16f80f6198dbe5c2ab10e6500fc1a3f166cd0
ranjit03 <ranjitendurthi03@gmail.com>
2019-02-22 13:23:19 +1300

    added images to seed data

--- seeds/found.js
+++ seeds/found.js
@@ -1,13 +1,13 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('found').del()
-    .then(function () {
+  return knex("found")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('found').insert([
-        {id: 1, species: 'labrador', photo: 'url', user_id: 1},
-        {id: 2,  species: 'tabby', photo: 'url', user_id: 2},
-        {id: 3, species: 'pointer', photo: 'url', user_id: 3}
+      return knex("found").insert([
+        { id: 1, species: "labrador", photo: "/images/puppy1", user_id: 1 },
+        { id: 2, species: "tabby", photo: "/images/puppy2", user_id: 2 },
+        { id: 3, species: "pointer", photo: "/images/puppy3", user_id: 3 }
       ]);
     });
 };
--- seeds/lost.js
+++ seeds/lost.js
@@ -1,13 +1,31 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('lost').del()
-    .then(function () {
+  return knex("lost")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('lost').insert([
-        {id: 1, name: 'bob', species: 'labrador', photo: 'url', user_id: 1},
-        {id: 2,  name: 'alice', species:'tabby', photo: 'url', user_id: 2},
-        {id: 3, name: 'worm', species: 'pointer', photo: 'url', user_id: 3}
+      return knex("lost").insert([
+        {
+          id: 1,
+          name: "bob",
+          species: "labrador",
+          photo: "/images/puppy4",
+          user_id: 1
+        },
+        {
+          id: 2,
+          name: "alice",
+          species: "tabby",
+          photo: "/images/puppy5",
+          user_id: 2
+        },
+        {
+          id: 3,
+          name: "worm",
+          species: "pointer",
+          photo: "/images/puppy6",
+          user_id: 3
+        }
       ]);
     });
 };
